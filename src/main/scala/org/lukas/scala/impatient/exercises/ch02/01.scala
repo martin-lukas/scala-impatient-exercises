ba08bacf5239126ed80ff2f60c6a6f25445b5a8b
Martin Lukas <martinlukas24@protonmail.com>
2023-06-17 22:14:05 +0100

    Refactor exercise with new test framework

--- src/main/scala/org/lukas/scala/impatient/exercises/ch02/01.scala
+++ src/main/scala/org/lukas/scala/impatient/exercises/ch02/01.scala
@@ -1,26 +1,16 @@
 package org.lukas.scala.impatient.exercises.ch02
 
 import org.lukas.scala.impatient.exercises.Console
-import scala.annotation.tailrec
-import scala.util.{Try, Success, Failure}
-import scala.util.control.NonFatal
+import org.lukas.scala.impatient.exercises.Test.runTests
 
-// IMPL
 def signum(num: Int): Int = num.sign
 
-// TESTS
 @main def main01(args: String*): Unit = {
-  def testSignum(): Unit = {
-    assert(signum(0) == 0)
-    assert(signum(13) == 1)
-    assert(signum(-13) == -1)
-  }
-
-  Try {
-    testSignum()
-    Console.info("All tests passed!")
-  } match {
-    case Success(_) => ()
-    case Failure(ex) => println(s"Some tests failed: ${ex.getMessage}")
-  }
+  runTests(
+    "signum should return sign of numbers" -> (() => {
+      assert(signum(0) == 0)
+      assert(signum(13) == 1)
+      assert(signum(-13) == -1)
+    })
+  )
 }
