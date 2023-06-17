b01c86beafb4d682634d6d87318869aa63f5605d
Martin Lukas <martinlukas24@protonmail.com>
2023-06-17 22:13:16 +0100

    Add log to console

--- src/main/scala/org/lukas/scala/impatient/exercises/Console.scala
+++ src/main/scala/org/lukas/scala/impatient/exercises/Console.scala
@@ -5,6 +5,8 @@ object Console {
   private val ANSI_RED = "\u001B[31m"
   private val ANSI_RESET = "\u001B[0m"
 
+  def log(msg: String): Unit = println(msg)
+
   def info(msg: String): Unit = println(ANSI_GREEN + msg + ANSI_RESET)
 
   def error(msg: String): Unit = println(ANSI_RED + msg + ANSI_RESET)
