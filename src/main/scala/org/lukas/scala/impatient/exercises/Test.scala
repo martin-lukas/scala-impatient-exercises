47634c7ef393e6e15747b02969cef9ae04d3040b
Martin Lukas <martinlukas24@protonmail.com>
2023-06-17 22:49:30 +0100

    Improve test framework

--- src/main/scala/org/lukas/scala/impatient/exercises/Test.scala
+++ src/main/scala/org/lukas/scala/impatient/exercises/Test.scala
@@ -6,15 +6,24 @@ import scala.util.{Failure, Success, Try}
 import scala.util.control.NonFatal
 
 object Test {
-  def runTests(tests: (String, () => Unit)*): Unit = tests.foreach(fn => {
-    Try {
-      Console.log(s"Test: ${fn._1}")
-      //noinspection ScalaUnusedExpression
-      fn._2()
-      Console.info("Test passed!")
-    } match {
-      case Success(_) => ()
-      case Failure(ex) => println(s"Some tests failed: ${ex.getMessage}")
+  def runTests(name: String, tests: (String, () => Unit)*): Unit = {
+    Console.log(s"Test: $name")
+    val results = tests.map(fn => {
+      Try {
+        Console.log(s"Case: ${fn._1}")
+        //noinspection ScalaUnusedExpression
+        fn._2()
+      }.recoverWith {ex =>
+        Console.error(s"Case ${ex.getMessage}")
+        Failure(ex)
+      }
+    })
+    val failures = results.count(_.isFailure)
+    if (failures == 0) {
+      Console.info("All tests passed!")
+    } else {
+      Console.error(s"${tests.size - failures} of ${tests.size} tests passed.")
     }
-  })
+    println()
+  }
 }
--- src/main/scala/org/lukas/scala/impatient/exercises/ch02/01.scala
+++ src/main/scala/org/lukas/scala/impatient/exercises/ch02/01.scala
@@ -7,10 +7,15 @@ def signum(num: Int): Int = num.sign
 
 @main def main01(args: String*): Unit = {
   runTests(
-    "signum should return sign of numbers" -> (() => {
+    "signum",
+    "should return 0 for 0" -> (() => {
       assert(signum(0) == 0)
+    }),
+    "should return 1 for positive numbers" -> (() => {
       assert(signum(13) == 1)
+    }),
+    "should return -1 for negative numbers" -> (() => {
       assert(signum(-13) == -1)
-    })
+    }),
   )
 }
